48e210a561001c4c89eb4da90f812c4ea674cc57
Jesse Chan <jc@linux.com>
2020-04-11 11:04:16 +0000

    drivers: gpu/msm, esoc: allow CORESIGHT to be disabled
    Signed-off-by: Jesse Chan <jc@linux.com>

--- drivers/gpu/msm/adreno.h
+++ drivers/gpu/msm/adreno.h
@@ -855,6 +855,7 @@ struct adreno_vbif_snapshot_registers {
 	const int count;
 };
 
+#ifdef CONFIG_CORESIGHT
 /**
  * struct adreno_coresight_register - Definition for a coresight (tracebus)
  * debug register
@@ -908,7 +909,7 @@ struct adreno_coresight {
 	void (*write)(struct kgsl_device *device,
 		unsigned int offsetwords, unsigned int value);
 };
-
+#endif
 
 struct adreno_irq_funcs {
 	void (*func)(struct adreno_device *, int);
